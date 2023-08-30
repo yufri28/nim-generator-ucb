9eead8e8524416289949e937c594612b264a2048
Yufri Luttu <yufridonluttu@gmail.com>
2023-08-30 10:07:04 +0700

    setup sb-admin

--- application/config/config.php
+++ application/config/config.php
@@ -23,7 +23,7 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 | a PHP script and you can easily do that on your own.
 |
 */
-$config['base_url'] = '';
+$config['base_url'] = 'http://localhost/nim-generator/';
 
 /*
 |--------------------------------------------------------------------------
@@ -530,3 +530,4 @@ $config['rewrite_short_tags'] = FALSE;
 | Array:		array('10.0.1.200', '192.168.5.0/24')
 */
 $config['proxy_ips'] = '';
+date_default_timezone_set('asia/jakarta');
